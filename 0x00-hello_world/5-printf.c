802dc3f2f21b0358ad58993256e46d3d7150063e
Mk1806 <91425253+Mk1806@users.noreply.github.com>
2022-09-08 20:12:52 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ *Return : 0
  */
- 
-int main (void)
+ int main (void)
 {
-	prtintf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	prtintf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
