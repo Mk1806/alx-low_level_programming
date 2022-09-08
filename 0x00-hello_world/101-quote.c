9593f37504fbd76566939bd7d4657f9d17de9bd0
Mk1806 <91425253+Mk1806@users.noreply.github.com>
2022-09-08 20:59:19 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entry point
+ * main - prints to the standard error
+ 
  * Return: 1
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (2, ch, sizeof(ch));
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
