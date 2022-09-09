890ced507ef12d1e5c872251de43ac04acff1508
Mk1806 <91425253+Mk1806@users.noreply.github.com>
2022-09-09 22:19:45 +0300

    Update 9-print_comb.c
    My tenth commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ * main - print possible combination 0-9
+ *
+ * Return: zero
  */
 int main(void)
 {
-int ch;
-for (ch = 48; ch <= 57; ch++)
+int i;
+
+for (i = '0'; i <= '9'; i++)
 {
-
-putchar(ch);
-if (ch != 57)
-{
-putchar(44);
-putchar(32);
+putchar(i);
+if (i == '9')
+break;
+putchar(',');
+putchar(' ');
 }
-}
-
-putchar(10); /* this is an ascii code for new line*/
-
+putchar('\n');
 return (0);
 }
