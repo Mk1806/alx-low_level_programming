faccf89447c15091bc165d3bc7fe53df83015509
Mk1806 <91425253+Mk1806@users.noreply.github.com>
2022-09-09 21:35:15 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,16 @@
  **/
 int main(void)
 {
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch);
-		}
-	}
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
 
-	putchar(10); /* this is an ascii code for new line*/
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
