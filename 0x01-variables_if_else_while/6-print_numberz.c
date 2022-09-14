069750134d5748703f38539874749a6a58ff008c
Mk1806 <91425253+Mk1806@users.noreply.github.com>
2022-09-14 23:15:31 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
- */
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ **/
 int main(void)
 {
-int ch;
-for (ch = 48; ch <= 57; ch++)
-{
+int num;
 
-putchar(ch);
-}
+for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
 
-putchar(10); /* this is an ascii code for new line*/
+putchar('\n');
 
 return (0);
 }
